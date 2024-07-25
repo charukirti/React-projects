229e80911ca283f26df2f87c862f4c35e9848a7b
charukirti <charukirticc112002@gmail.com>
2024-07-25 15:42:29 +0530

    created task component

--- Todo list/src/components/Task.jsx
+++ Todo list/src/components/Task.jsx
@@ -1,8 +1,17 @@
-export default function Task (){
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCircleCheck, faPenToSquare, faTrashCan } from "@fortawesome/free-regular-svg-icons"
+
+export default function Task ({task}){
     return (
-        <>
-            
-        </>
+        <div className="task__list--item">
+            <p className="task__name">{task.taskName}</p>
+
+            <div className="btn__container">
+            <FontAwesomeIcon icon={faCircleCheck} className="btn__container--complete"/>
+            <FontAwesomeIcon icon={faPenToSquare} className="btn__container--edit"/>
+            <FontAwesomeIcon icon={faTrashCan} className="btn__container--delete"/>
+            </div>
+        </div>
     )
 }
 
