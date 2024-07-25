f8a34931d845ee272410f3602e35632a7dabf9b0
charukirti <charukirticc112002@gmail.com>
2024-07-25 15:43:09 +0530

    created task list component

--- Todo list/src/components/TaskList.jsx
+++ Todo list/src/components/TaskList.jsx
@@ -1,8 +1,10 @@
-export default function TaskList (){
+import Task from "./Task";
+
+export default function TaskList ({tasks}){
     return (
-        <>
-            
-        </>
+        <section className="task__list">
+            {tasks.map((task) => <Task task = {task} key={task.id}/>)}
+        </section>
     )
 }
 
