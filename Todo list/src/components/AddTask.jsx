1bfb04ae32ef17810aa35771c643c5c753c5be9b
charukirti <charukirticc112002@gmail.com>
2024-07-25 15:31:00 +0530

    add task component

--- Todo list/src/components/AddTask.jsx
+++ Todo list/src/components/AddTask.jsx
@@ -1,8 +1,9 @@
 export default function AddTask (){
     return (
-        <>
-            
-        </>
+        <form className="add__task">
+            <input type="text" placeholder="what you want to do...?"/>
+            <button>Add Task</button>
+        </form>
     )
 }
 
