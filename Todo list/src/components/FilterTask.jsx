441a86731aee449e93cbad86aba7596c6b281974
charukirti <charukirticc112002@gmail.com>
2024-07-25 15:41:33 +0530

    created filter task component

--- Todo list/src/components/FilterTask.jsx
+++ Todo list/src/components/FilterTask.jsx
@@ -1,8 +1,9 @@
-export default function FilterTask (){
-    return (
-        <>
-            
-        </>
-    )
+export default function FilterTask() {
+  return (
+    <section className="filter__tasks">
+      <button className="filter__task--all">All</button>
+      <button className="filter__task--active">Active</button>
+      <button className="filter__task--complete">Completed</button>
+    </section>
+  );
 }
-
