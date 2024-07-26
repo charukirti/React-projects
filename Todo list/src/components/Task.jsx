e1bf7aa89dd935d434e985687b88cb6013a91097
charukirti <charukirticc112002@gmail.com>
2024-07-26 23:44:50 +0530

    added event listionars

--- Todo list/src/components/Task.jsx
+++ Todo list/src/components/Task.jsx
@@ -1,17 +1,34 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faCircleCheck, faPenToSquare, faTrashCan } from "@fortawesome/free-regular-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleCheck,
+  faPenToSquare,
+  faTrashCan,
+} from "@fortawesome/free-regular-svg-icons";
 
-export default function Task ({task}){
-    return (
-        <div className="task__list--item">
-            <p className="task__name">{task.taskName}</p>
+export default function Task({ task, toggleComplete, toggleDelete, toggleEdit }) {
+  return (
+    <div className="task__list--item">
+      <p className={`task__name ${task.isComplete ? "completed" : ""}`}>
+        {task.taskName}
+      </p>
 
-            <div className="btn__container">
-            <FontAwesomeIcon icon={faCircleCheck} className="btn__container--complete"/>
-            <FontAwesomeIcon icon={faPenToSquare} className="btn__container--edit"/>
-            <FontAwesomeIcon icon={faTrashCan} className="btn__container--delete"/>
-            </div>
-        </div>
-    )
+      <div className="btn__container">
+        <FontAwesomeIcon
+          icon={faCircleCheck}
+          className="btn__container--complete"
+          onClick={() => toggleComplete(task.id)}
+        />
+        <FontAwesomeIcon
+          icon={faPenToSquare}
+          className="btn__container--edit"
+          onClick={() => toggleEdit(task.id)}
+        />
+        <FontAwesomeIcon
+          icon={faTrashCan}
+          className="btn__container--delete"
+          onClick={() => toggleDelete(task.id)}
+        />
+      </div>
+    </div>
+  );
 }
-
