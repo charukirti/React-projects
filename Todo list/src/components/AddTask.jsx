d7966221ca292d13670bff72a934532545f8d187
charukirti <charukirticc112002@gmail.com>
2024-07-26 23:46:50 +0530

    creadted add task function

--- Todo list/src/components/AddTask.jsx
+++ Todo list/src/components/AddTask.jsx
@@ -1,9 +1,25 @@
-export default function AddTask (){
-    return (
-        <form className="add__task">
-            <input type="text" placeholder="what you want to do...?"/>
-            <button>Add Task</button>
-        </form>
-    )
-}
+import { useState } from "react";
 
+export default function AddTask({ addTask }) {
+  const [task, setTask] = useState("");
+
+  // function to handle task
+  function handleTaskSubmit(e) {
+    e.preventDefault();
+    if (task) {
+      addTask(task);
+      setTask("");
+    }
+  }
+  return (
+    <form className="add__task" onSubmit={handleTaskSubmit}>
+      <input
+        type="text"
+        placeholder="what you want to do...?"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button>Add Task</button>
+    </form>
+  );
+}
