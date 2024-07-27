34c0c25cc71a009ec81d0ede12937aaea6f82e1b
charukirti <charukirticc112002@gmail.com>
2024-07-27 12:01:23 +0530

    filter task function implemented

--- Todo list/src/components/FilterTask.jsx
+++ Todo list/src/components/FilterTask.jsx
@@ -1,9 +1,21 @@
-export default function FilterTask() {
+export default function FilterTask({ updateFilter }) {
   return (
     <section className="filter__tasks">
-      <button className="filter__task--all">All</button>
-      <button className="filter__task--active">Active</button>
-      <button className="filter__task--complete">Completed</button>
+      <button className="filter__task--all" onClick={() => updateFilter("all")}>
+        All
+      </button>
+      <button
+        className="filter__task--active"
+        onClick={() => updateFilter("active")}
+      >
+        Active
+      </button>
+      <button
+        className="filter__task--complete"
+        onClick={() => updateFilter("completed")}
+      >
+        Completed
+      </button>
     </section>
   );
 }
