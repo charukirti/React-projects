6f55d877e7f2f2cb3f7a1bb045aea6f190825538
charukirti <charukirticc112002@gmail.com>
2024-08-19 23:12:18 +0530

    added logic to the code

--- WeatherApp/src/components/WeatherData.jsx
+++ WeatherApp/src/components/WeatherData.jsx
@@ -1,8 +1,30 @@
-export default function WeatherData (){
-    return (
-        <>
-            
-        </>
-    )
-}
+import humidity__icon from "../assets/humidity.png";
+import wind__icon from "../assets/wind.png";
 
+export default function WeatherData({ details }) {
+  return (
+    <div className="weather-app__data">
+      <img src={details.icon} alt="" className="weather-app__icon" />
+      <p className="weather-app__temp">{details.tempreture}°c</p>
+      <p className="weather-app__location">{details.location}</p>
+
+      <div className="weather-app__details ">
+        <div className="col">
+          <img src={humidity__icon} alt="" />
+          <div>
+            <p>{details.humidity}%</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+
+        <div className="col">
+          <img src={wind__icon} alt="" />
+          <div>
+            <p>{details.windSpeed} Km/h</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
