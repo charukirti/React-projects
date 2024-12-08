1c85bce280737453dbc0da4cbfb61c0fe0b06474
charukirti <charukirticc112002@gmail.com>
2024-12-08 23:40:31 +0530

    styled product list and product card.

--- Simple_add_to_cart/src/pages/ProductList.jsx
+++ Simple_add_to_cart/src/pages/ProductList.jsx
@@ -1,11 +1,14 @@
 import { products } from "../data/Products";
 import ProductCard from "../ui/ProductCard";
 
-export default function ProductList (){
-    return (
-        <article>
-            {products.map(product => <ProductCard key={product.id} product={product} />)} 
-        </article>
-    )
+export default function ProductList() {
+  return (
+    <section>
+      <div className="flex flex-col items-center my-8 gap-5 sm:grid sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 sm:place-items-center mx-20">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </section>
+  );
 }
-
--- Simple_add_to_cart/src/ui/ProductCard.jsx
+++ Simple_add_to_cart/src/ui/ProductCard.jsx
@@ -1,8 +1,24 @@
-export default function ProductCard ({product}){
-    return (
-        <div>
-            <img src={product.thumbnail} alt="" />
-        </div>
-    )
-}
+export default function ProductCard({ product }) {
+  return (
+    <div className="flex flex-col items-center  w-60 border border-stone-200 rounded-lg py-2 px-3">
+      {/* image */}
+      <div>
+        <img src={product.image} alt={product.title} />
+      </div>
+      <div className="py-2 text-center">
+        {/* brand and model */}
 
+        <p className="text-base mb-5">{product.model}</p>
+
+        {/* price */}
+        <div className=" flex items-center justify-between gap-8">
+          <p>₹ {product.price}</p>
+          <button className="px-3 py-1 bg-yellow-400 rounded-lg text-base">
+            Add to cart
+          </button>
+        </div>
+      </div>
+      <div className="px-2"></div>
+    </div>
+  );
+}
